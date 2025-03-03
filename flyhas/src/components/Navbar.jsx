08d6536987d65f903043e0943b331e2cd37196e9
Berkedgnn <berkedogan_2001@hotmail.com>
2025-03-03 10:55:37 +0300

    v2

--- flyhas/src/components/Navbar.jsx
+++ flyhas/src/components/Navbar.jsx
@@ -12,33 +12,34 @@ const Navbar = () => {
   };
 
   const handleMenuClose = () => {
+    z <
     setAnchorEl(null);
   };
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: "#1976d2" }}> 
+    <AppBar position="static" sx={{ backgroundColor: "#1976d2" }}>
       <Toolbar>
         {/* LOGO and Title */}
         <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
-        <img src={logo} alt="Logo" style={{ width: "90px", height: "90px", marginRight: "30px" }} />
-        <Typography variant="h6" sx={{ fontWeight: "300", fontFamily: "'Alfa Slab One', sans-serif", fontSize: "1.5rem", color: "white"  }}>FLYHAS Flight Reservation</Typography>
+          <img src={logo} alt="Logo" style={{ width: "90px", height: "90px", marginRight: "30px" }} />
+          <Typography variant="h6" sx={{ fontWeight: "300", fontFamily: "'Alfa Slab One', sans-serif", fontSize: "1.5rem", color: "white" }}>FLYHAS Flight Reservation</Typography>
         </Box>
 
         {/* Menü Butonu*/}
-        <IconButton 
-          edge="start" 
-          color="inherit" 
-          aria-label="menu" 
-          onClick={handleMenuOpen} 
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={handleMenuOpen}
           sx={{ display: { xs: "block", md: "none" } }}
         >
           <MenuIcon />
         </IconButton>
 
         {/* Açılır Menü */}
-        <Menu 
-          anchorEl={anchorEl} 
-          open={Boolean(anchorEl)} 
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
           onClose={handleMenuClose}
           anchorOrigin={{ vertical: "top", horizontal: "right" }}
           transformOrigin={{ vertical: "top", horizontal: "right" }}
@@ -52,8 +53,8 @@ const Navbar = () => {
           <Button color="inherit">Services</Button>
           <Button color="inherit">City Guide</Button>
           <Button sx={{ backgroundColor: "#001F5B", color: "white", "&:hover": { backgroundColor: "#000E3B" } }}>
-  Sign In
-</Button>
+            Sign In
+          </Button>
 
         </Box>
       </Toolbar>
